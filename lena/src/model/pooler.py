207a55889c0962716b2f33430693e0ccc19a688e
Mohammadhossein (MH) Amouei <71391890+mhamouei@users.noreply.github.com>
2025-10-02 14:55:17 -0400

    Update pooler.py

--- lena/src/model/pooler.py
+++ lena/src/model/pooler.py
@@ -4,38 +4,32 @@ import torch.nn.functional as F
 
 class Pooler(nn.Module):
     def __init__(self):
-        super(Pooler, self).__init__()
-        
-        self.norm = nn.LayerNorm(3 * 4096)
-        self.attention = nn.MultiheadAttention(4096, num_heads=32, batch_first=True)
-        self.fc1 = nn.Linear(3 * 4096, 2 * 4096)
-        self.fc2 = nn.Linear(2 * 4096, 4096)
-        self.fc3 = nn.Linear(4096, 4096)
-        self.fc4 = nn.Linear(4096, 4096)
-        self.fc5 = nn.Linear(4096, 4096)
+        super(Lena, self).__init__()
+
+        self.projection = nn.Sequential(
+            nn.Linear(2 * 4096, 2 * 4096),
+            nn.ELU(),
+            nn.Linear(2 * 4096, 4096),
+            nn.ELU(),
+            nn.Linear(4096, 4096),
+            nn.BatchNorm1d(4096),
+            nn.ELU(),
+            nn.Linear(4096, 4096, bias=False)
+        )
 
         self._initialize_weights()
 
 
-    def forward(self, vec):
-        x1 = self.norm(vec)
-        if x1.dim() == 1:
-            x1 = x1.unsqueeze(0)
-        x1 = x1.unsqueeze(-1)
-        x1 = x1.view(x1.size(0), 3, 4096)
-        x1, _ = self.attention(x1, x1, x1)
-        x1 = x1.flatten(start_dim=1)
-        x1 = F.elu(self.fc1(x1))
-        x1 = F.elu(self.fc2(x1))
-        x1 = F.elu(self.fc3(x1))
-        x1 = F.elu(self.fc4(x1))
-        x1 = self.fc5(x1)
-        return x1
+    def forward(self, x):
+        emb = self.projection(x)
+        return emb
     
     
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Linear):
-                nn.init.xavier_uniform_(m.weight)
+                gain = nn.init.calculate_gain('leaky_relu', param=1.0)
+                nn.init.kaiming_normal_(m.weight, a=1.0, mode='fan_in', nonlinearity='leaky_relu')
+                m.weight.data.mul_(gain)
                 if m.bias is not None:
                     nn.init.zeros_(m.bias)
